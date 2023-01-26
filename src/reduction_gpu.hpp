8573abf4a34493974bfd0d860c0f1fca0791186a
shu65 <dolphinripple@gmail.com>
2023-01-26 19:42:38 +0900

    fix

--- src/reduction_gpu.hpp
+++ src/reduction_gpu.hpp
@@ -13,4 +13,3 @@ int reduce_gpu_v6(const int *d_data, int *h_tmp_out, int *d_tmp_out, size_t n, i
 int reduce_gpu_v7(const int *d_data, int *h_tmp_out, int *d_tmp_out, size_t n, int n_blocks, int n_threads);
 int reduce_gpu_v8(const int *d_data, int *h_tmp_out, int *d_tmp_out, size_t n, int n_blocks, int n_threads);
 int reduce_gpu_v9(const int *d_data, int *h_tmp_out, int *d_tmp_out, size_t n, int n_blocks, int n_threads);
-int reduce_gpu_v10(const int *d_data, int *h_tmp_out, int *d_tmp_out, size_t n, int n_blocks, int n_threads);
